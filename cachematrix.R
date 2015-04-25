b5725a5081bf95ea6a7aa390ff42610a129825f0
ofc587a87 <ofc587a87@gmail.com>
2015-04-25 14:01:37 +0200

    add test method

--- cachematrix.R
+++ cachematrix.R
@@ -48,7 +48,59 @@ cacheSolve <- function(x, ...) {
     }
     
     # it's not cached, solve ans save it
-    result <- solve(x$get())
+    result <- solve(x$get(), ...)
     x$setInverse(result)
     result;
+}
+
+#Internal function to test development
+testSolve <- function() {
+    
+    set.seed(Sys.time())
+    
+    ncols1<-floor(runif(1, 4, 300));
+    ncols2<-floor(runif(1, 4, 300));
+    
+    # creates sample matrices
+    message(paste("Generating matrix 1 of" , ncols1 , "cols/rows"))
+    M1 <- replicate(ncols1, floor(runif(ncols1, 0, 15)))
+    message(paste("Generating matrix 2 of" , ncols2 , "cols/rows"))
+    M2 <- replicate(ncols2, floor(runif(ncols2, 0, 100)))
+    
+    #create cache objects
+    cachedM1 <- makeCacheMatrix(M1)
+    cachedM2 <- makeCacheMatrix(M2)
+    
+    #verify data
+    if(!identical(M1, cachedM1$get())) { stop("M1 is not identical"); }
+    if(!identical(M2, cachedM2$get())) { stop("M2 is not identical"); }
+    if(!is.null(cachedM1$getInverse())) { stop("M1 inverse is not null at start"); }
+    if(!is.null(cachedM2$getInverse())) { stop("M2 inverse is not null at start"); }
+    if(identical(cachedM1$get(), cachedM2$get())) { stop("cached M1 and M2 should not be identical")}
+    
+    message("Generated matrix OK.")
+    
+    #solve inverses
+    message("Generating inverse 1...")
+    cacheSolve(cachedM1);
+    message("Generating inverse 2...")
+    cacheSolve(cachedM2);
+    message("checking...")
+    
+    #verify cached results
+    if(!identical(M1, cachedM1$get())) { stop("M1 is not identical after solve"); }
+    if(!identical(M2, cachedM2$get())) { stop("M2 is not identical after solve"); }
+    if(identical(cachedM1$get(), cachedM2$get())) { stop("cached M1 and M2 should not be identical")}
+    if(is.null(cachedM1$getInverse())) { stop("M1 inverse is null after solve"); }
+    if(is.null(cachedM2$getInverse())) { stop("M2 inverse is null after solve"); }
+    if(identical(cachedM1$getInverse(), cachedM2$getInverse())) { error("cached inversed M1 and M2 should not be identical")}
+    
+    #verify results
+    if(!identical(solve(M1), cachedM1$getInverse())) { stop("M1 inverse is not right"); }
+    if(!identical(solve(M2), cachedM2$getInverse())) { stop("M2 inverse is not right"); }
+    if(!identical(M1, round(solve(cachedM1$getInverse())))) { stop("M1 inverse is not right (double inverse check)"); }
+    if(!identical(M2, round(solve(cachedM2$getInverse())))) { stop("M2 inverse is not right (double inverse check)"); }
+
+    # All OK    
+    message("TEST OK!!!!")
 }
